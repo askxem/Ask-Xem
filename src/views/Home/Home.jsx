1b4190dbbc1feab5becb67451d03b4ea4b2ae303
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-18 10:49:49 -0800

    add landing page with links to authorization and continue as a guest

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 
 export default function Home() {
   return (
     <div>
-      Home View
+      <section>
+      <h1>Welcome to Ask Xem</h1>
+      <p>Xem will be your guide as you learn explore the site</p> {'....rest of welcome message'}
+      </section>
+      <Link to="/auth">Login or Sign Up</Link>
+      <Link to='/select'>Continue as Guest</Link>
     </div>
   )
 }
