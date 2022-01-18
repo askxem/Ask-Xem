23bdd1198a6d6d714790afd0da8b61d209f6d599
Michael Rider <mikepdxrider@gmail.com>
2022-01-18 14:44:45 -0800

    adds guide sans user context, includes snapshot test

--- src/components/Guide/Guide.jsx
+++ src/components/Guide/Guide.jsx
@@ -1,8 +1,24 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-export default function Guide() {
+export default function Guide({text}) {
+  //LATER:
+    // pull users guide from user context
+  const [userGuide, setUserGuide] = useState('lion');
+  
+
   return (
-    <div>
-      This is the guide component.
-    </div>
+    <article>
+      <div aria-label='guide text box'>
+        <pre>
+        {text}
+        </pre>
+      </div>
+      <img src={require(`../../assets/Icons/${userGuide}.png`)} alt={userGuide} aria-label='user guide'/>
+    </article>
   )
+}
+
+Guide.propTypes = {
+  text: PropTypes.string,
 }
