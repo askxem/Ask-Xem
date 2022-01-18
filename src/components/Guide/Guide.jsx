834f07077391c50b21f3c1eea31546f6832f5dc2
Michael Rider <mikepdxrider@gmail.com>
2022-01-18 14:49:59 -0800

    improves guide documentaion

--- src/components/Guide/Guide.jsx
+++ src/components/Guide/Guide.jsx
@@ -1,6 +1,11 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 
+/**
+ * 
+ * @param {string} text the guide's text box will display this value 
+ * @returns an image of the user's selected guide and a guide text box
+ */
 export default function Guide({text}) {
   //LATER:
     // pull users guide from user context
