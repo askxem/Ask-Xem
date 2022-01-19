9c06e557d61cdf1b2c728ae8fa4ec35c24852634
EmilyDSarani <emily.sarani@gmail.com>
2022-01-19 15:48:34 -0600

    lets see if testing passes this way

--- src/views/Gender/Gender.jsx
+++ src/views/Gender/Gender.jsx
@@ -29,3 +29,12 @@ export default function Gender() {
   )
 }
 
+//Fav Add Idea
+//const [Favs, setFavs] = useState('');
+//useEffect(async()=>{
+  // let savedFavs = await -put in localStorage-
+  //if(savedFavs){
+ // setFavs(savedFavs)  
+ // }
+//}, [])
+//<button onClick{() => }> </button>
