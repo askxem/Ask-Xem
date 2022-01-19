c44b7a5105206f1050c10d50e969d0e3a974cf15
Michael Rider <mikepdxrider@gmail.com>
2022-01-19 14:50:10 -0800

    disable nav if user doesnt exist

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { useAuth } from '../../context/AuthContext.jsx';
 import Journal from '../Journal/Journal.jsx';
 
 export default function Nav() {
     const [showJournal, setShowJournal] = useState(false);
+    const {user} = useAuth();
+
   return (
         //Nav links here!
         <article>
-            <button onClick={() => setShowJournal(true)}>Journal</button>
+            <button onClick={() => setShowJournal(true)} disabled={!user.id}>Journal</button>
             <Journal showJournal={showJournal} setShowJournal={setShowJournal}/>
         </article>
   );
