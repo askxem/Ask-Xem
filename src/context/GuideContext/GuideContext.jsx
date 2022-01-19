8f881451afa8b16c9fda52bf232c6498795ea172
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-19 12:41:21 -0800

    added Local Storage functionality to the guide context

--- src/context/GuideContext/GuideContext.jsx
+++ src/context/GuideContext/GuideContext.jsx
@@ -4,22 +4,36 @@ const GuideContext = createContext();
 
 function GuideProvider({children}) {
 
-    const [guide, setGuide] = useState('');
+  const hasSelectedGuide = getGuideLocal();
+
+  const [guide, setGuide] = useState(hasSelectedGuide ? hasSelectedGuide : '');
 
     return (
         <GuideContext.Provider value={{guide, setGuide}} >
             {children}
         </GuideContext.Provider>
     )
+
+function setGuideGlobal(guide) {
+  setGuideLocal(guide);
+  setGuide(guide);
+}
+
+function getGuideLocal() {
+  const stringyGuide = localStorage.getItem('GUIDE');
+  return JSON.parse(stringyGuide);
+}
+
+function setGuideLocal(guide) {
+  const stringyGuide = JSON.stringify(guide);
+  localStorage.setItem('GUIDE', stringyGuide)
 }
 
 function useGuide(){
     const context = useContext(GuideContext);
-
     if (context === undefined) throw new Error('Guide context not accessible outside of the guide provider.')
-
     return context;
 }
+}
 
-
-export { GuideProvider, useGuide }
+export { GuideProvider, useGuide, setGuideGlobal }
