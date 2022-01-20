b828bd566a433de4d3ba9a4128e753bd151cee7a
Michael Rider <mikepdxrider@gmail.com>
2022-01-19 20:17:01 -0800

    initial commit

--- src/components/Auth/AuthForm.test.jsx
+++ src/components/Auth/AuthForm.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render } from '@testing-library/react';
 import { Route, MemoryRouter } from 'react-router-dom';
 import AuthForm from './AuthForm.jsx'
 
