39e47ab3fa58e68db9bd760c365d64c959939fcd
diyanamendoza <diyana.amp@pm.me>
2022-01-21 08:25:22 -0800

    Some minor edits

--- src/utils/retrieveGuideText/retrieveGuideText.js
+++ src/utils/retrieveGuideText/retrieveGuideText.js
@@ -16,11 +16,11 @@ export default async function retrieveGuideText(cardCategory, cardAnimal, cardTi
             case 'rat': 
                 return 'My friend rat has been deadnamed. We try not to deadname our friends!'
             case 'raccoon': 
-                return 'My friend raccoon has gender dysphoria!'
+                return 'I\'m so glad my friend raccoon has found gender congruence. They feel happy that the way they\'re seen and named matches how they feel.'
             case 'octopus': 
-                return 'What\'s your gender identity?'
+                return 'What\'s your gender identity? Feel free to reflect in your journal!'
             case 'llama': 
-                return 'What\'s your gender expression?'
+                return 'How do you express your gender?'
             default:
                 return `My friend ${cardAnimal} identifies as ${cardTitle}.`
         }
