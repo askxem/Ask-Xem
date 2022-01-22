47c897ac0a0de0b55ad5d368f927b6d394aeea06
Michael Rider <mikepdxrider@gmail.com>
2022-01-21 16:06:36 -0800

    updates useForm hook documentation

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 /**
  * Called within AuthForm component. Can be used with any form component. 
  * @param {object} intialValue properties must correspond with input name attributes. 
- * @returns an array containing current form state object and handleChange fn for inputs.
+ * @returns an array containing current form state object, a handleChange fn for inputs, an error message, and a setError message fn.
  */
 export default function useForm(intialValue) {
     const [formState, setFormState] = useState({...intialValue});
