721c905f487d6f096ef9d88dcf08edc82fddd4e4
EmilyDSarani <emily.sarani@gmail.com>
2022-01-21 21:38:17 -0600

    console log gone

--- src/services/client.js
+++ src/services/client.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-console.log(process.env.SUPABASE_URL)
 export const client = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
