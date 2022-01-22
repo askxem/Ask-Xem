a7410e0eeb351e9895e1044873a5832e2cb93e68
EmilyDSarani <emily.sarani@gmail.com>
2022-01-21 21:27:17 -0600

    working test

--- src/services/client.js
+++ src/services/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
+console.log(process.env.SUPABASE_URL)
 export const client = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
