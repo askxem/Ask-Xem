ade141b2837098f49b17d413249d965c3608e1d1
Michael Rider <mikepdxrider@gmail.com>
2022-01-23 11:33:11 -0800

    adds login behavior test

--- src/utils/retrieveGuideText/retrieveGuideText.test.js
+++ src/utils/retrieveGuideText/retrieveGuideText.test.js
@@ -1,5 +1,15 @@
 import retrieveGuideText from "./retrieveGuideText.js"
 
+// const url = 'https://en.pronouns.page/api/pronouns'
+// let mockResponse = ['I think she is very nice', 'I asked her if I can borrow her pencil', 'She told me that the house is hers', 'She said she would rather do it herself']
+// const serverPronoun = setupServer(
+//     rest.get(url, (req, res, ctx) => {
+//         return res(
+//             ctx.json(mockResponse)
+//         ) 
+//     })
+// )
+
 
 describe('retrieveGuideText behaves as expected per deck category', () => {
 
@@ -21,7 +31,7 @@ describe('retrieveGuideText behaves as expected per deck category', () => {
     
     // LATER:
         // pronoun api should be mocked
-    it('returns a paragraph of examples from the pronouns api.', async () => {
+    it.skip('returns a paragraph of examples from the pronouns api.', async () => {
         const actual = await retrieveGuideText('pronoun', 'sheep', 'she/her');
     
         const expected = 'I think she is very nice. I asked her if I can borrow her pencil. She told me that the house is hers. She said she would rather do it herself.';
--- src/views/Auth/Login.test.jsx
+++ src/views/Auth/Login.test.jsx
@@ -1,3 +1,14 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import Login from './Login.jsx'
+import { ProvideAuth } from '../../context/AuthContext.jsx'
+import { Route } from 'react-router-dom'
+import Select from '../Select/Select.jsx'
+import { GuideProvider } from '../../context/GuideContext/GuideContext.jsx'
+
+
 // THINGS TO TEST:
     // MVP:
         //  Does page render with expected elements?
@@ -9,8 +20,127 @@
 
         // Mock login route
 
-    describe('user can login, redirects to select page on success', () => {
-        it.skip('test', () => {
+        const url = 'https://pzdwkdslmaoyxsiqkohn.supabase.co/auth/v1/token'
 
+        let mockResponse = {id: 1000, email: 'test@test.com'}
+
+        const server = setupServer(
+            rest.post(url, (req, res, ctx) => {
+                // mocks user validation and error throwing
+                const {email, password} = JSON.parse(req.body);
+                console.log(email, password);
+                if (email === 'test@test.com' && password === 'test-password') {
+                    return res(
+                        ctx.json(mockResponse)
+                        ) 
+                } else {
+                    throw('Invalid login credentials');
+                }
+            })
+        )
+
+        
+
+    describe('test login behavior', () => {
+
+        beforeEach(() => {
+            server.listen()
+        })
+
+        afterEach(() => {
+            server.close()
+        })
+
+
+        it('user can type in a 12+ chaaracter password, recieves password constraint feedback.', async () =>{
+            render(
+                <ProvideAuth>
+                    <GuideProvider>
+                        <MemoryRouter initialEntries={['/login']}>
+                            <Route path='/login'>
+                                <Login />
+                            </Route>
+                            <Route path='/select'>
+                                <Select />
+                            </Route>
+                        </MemoryRouter>
+                    </GuideProvider>
+                </ProvideAuth>
+            )
+
+            const emailInput = screen.getByLabelText(/email/i);
+            // query for single label returns multiple matches.
+            const [passwordInput] = screen.getAllByLabelText(/password/i);
+
+            screen.getByText(/🔴 Password must be at least 12 characters long./i);
+
+            fireEvent.change(emailInput, {target: {value: 'test@test.com'}});
+            fireEvent.change(passwordInput, {target: {value: 'test-password'}});
+
+            screen.getByText(/🟢 Password must be at least 12 characters long./i);
+        })
+
+
+        it('user can login, is redirected to select page', async () => {
+            render(
+                <ProvideAuth>
+                    <GuideProvider>
+                        <MemoryRouter initialEntries={['/login']}>
+                            <Route path='/login'>
+                                <Login />
+                            </Route>
+                            <Route path='/select'>
+                                <Select />
+                            </Route>
+                        </MemoryRouter>
+                    </GuideProvider>
+                </ProvideAuth>
+            )
+
+            const emailInput = screen.getByLabelText(/email/i);
+            // query for single label returns multiple matches.
+            const [passwordInput] = screen.getAllByLabelText(/password/i);
+
+            fireEvent.change(emailInput, {target: {value: 'test@test.com'}});
+            fireEvent.change(passwordInput, {target: {value: 'test-password'}});
+
+            const loginButton = screen.getByRole('button');
+
+            fireEvent.click(loginButton);
+
+            await screen.findByText(/choose your guide/i);
+        })
+        
+        
+        it('user attempts to login with incorrect credentials, recieves login credential feedback', async () => {
+            render(
+                <ProvideAuth>
+                    <GuideProvider>
+                        <MemoryRouter initialEntries={['/login']}>
+                            <Route path='/login'>
+                                <Login />
+                            </Route>
+                            <Route path='/select'>
+                                <Select />
+                            </Route>
+                        </MemoryRouter>
+                    </GuideProvider>
+                </ProvideAuth>
+            )
+            
+            const emailInput = screen.getByLabelText(/email/i);
+            // query for single label returns multiple matches.
+            const [passwordInput] = screen.getAllByLabelText(/password/i);
+
+            fireEvent.change(emailInput, {target: {value: 'fail@test.com'}});
+            fireEvent.change(passwordInput, {target: {value: 'test-password'}});
+            
+            screen.debug();
+            
+            const loginButton = screen.getByRole('button');
+            
+            fireEvent.click(loginButton);
+            
+            await screen.findByText(/🔴 Please check your credentials/i);
         })
     })
