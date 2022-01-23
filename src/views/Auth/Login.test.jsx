8e9d74bdf9d67ece548711739b9ffab0d6e79268
Michael Rider <mikepdxrider@gmail.com>
2022-01-23 08:18:39 -0800

    creates auth test files, adds loader aria label, all snapshots up to date

---
+++ src/views/Auth/Login.test.jsx
@@ -0,0 +1,16 @@
+// THINGS TO TEST:
+    // MVP:
+        //  Does page render with expected elements?
+        //  User can login, which redirects the user to select page.
+    // STRETCH: 
+        //  User fails to login, displays 'Please check credentials.'
+        //  User clicks 'already have an account?', redirects user to signup page.
+        //  When the password length reaches 12+, password contstrain text updates.
+
+        // Mock login route
+
+    describe('user can login, redirects to select page on success', () => {
+        it.skip('test', () => {
+
+        })
+    })
---
+++ src/views/Auth/Signup.test.jsx
@@ -0,0 +1,16 @@
+// THINGS TO TEST:
+    // MVP:
+        //  Does page render with expected elements?
+        //  User can login, which redirects the user to select page.
+    // STRETCH: 
+        //  User fails to signup, displays apppropriate errors
+        //  User clicks 'Need an account?', redirects user to login page.
+        //  When the password length reaches 12+, password contstraint text updates.
+
+        // Mock, signup route
+
+        describe('user can signup, redirects to select page on success', () => {
+            it.skip('test', () => {
+    
+            })
+        })
