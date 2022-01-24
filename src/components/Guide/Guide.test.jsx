b9e30e0c2a94c09de6a1f32368c841072283bd6e
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-23 16:50:25 -0800

    added guide component behavioral testing, need to split behavioral tests into the views after all merging

--- src/components/Guide/Guide.test.jsx
+++ src/components/Guide/Guide.test.jsx
@@ -1,5 +1,9 @@
-import { render } from "@testing-library/react"
-import { GuideProvider } from "../../context/GuideContext/GuideContext.jsx";
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { GuideProvider } from "../../context/GuideContext/GuideContext.jsx"
+import { DeckProvider } from "../../context/DeckContext/DeckContext.jsx"
+import Pronouns from "../../views/Pronouns/Pronouns.jsx"
+import Gender from "../../views/Gender/Gender.jsx"
 import Guide from "./Guide.jsx"
 
 it('renders guide component', () => {
@@ -10,4 +14,36 @@ it('renders guide component', () => {
     );
 
     expect(container).toMatchSnapshot();
+});
+
+it('when on the Pronouns page - should render the Pronouns guide text message', async() => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/pronouns']}>
+        <DeckProvider>
+          <GuideProvider>
+              <Pronouns />
+          </GuideProvider>
+        </DeckProvider>
+      </MemoryRouter>
+    );
+
+    const guideText = "This is the Pronoun Deck - click on a card to find out more! Visit all of my friends for a colorful surprise :)"
+
+    await screen.findByText(guideText);
+});
+
+it('when on the Gender page - should render the Gender guide text message', async() => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/gender']}>
+        <DeckProvider>
+          <GuideProvider>
+              <Gender />
+          </GuideProvider>
+        </DeckProvider>
+      </MemoryRouter>
+    );
+
+    const guideText = "This is the Gender Deck - click on a card to find out more! Visit all of my friends for a colorful surprise :)"
+
+    await screen.findByText(guideText);
 });
