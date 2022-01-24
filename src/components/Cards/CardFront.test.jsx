d3a8b9e7d774512ba2202b5c65d5a9fde5640e17
EmilyDSarani <emily.sarani@gmail.com>
2022-01-23 19:54:01 -0600

    front card snap is working properly

--- src/components/Cards/CardFront.test.jsx
+++ src/components/Cards/CardFront.test.jsx
@@ -3,11 +3,12 @@ import { MemoryRouter } from 'react-router';
 import { DeckProvider } from '../../context/DeckContext/DeckContext'
 import CardFront from './CardFront'
 
+const character = {title:'Sylvestor the Cat', animal: 'cat', image:'testURL'}
 it('renders front card component', () => {
     const { container } = render(
         <DeckProvider>
             <MemoryRouter>
-            <CardFront card='Sylvestor The Cat'/>
+            <CardFront card={character}/>
             </MemoryRouter>
         </DeckProvider>
     );
