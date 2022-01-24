6007fefaac05ca2200cbe06535e2aeb52f02d4e7
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-23 20:29:07 -0800

    added snapshot test for footer component

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom";
+import Footer from "./Footer.jsx"
+
+it('renders footer component', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    expect(container).toMatchSnapshot();
+});
