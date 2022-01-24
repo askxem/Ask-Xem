02e02ecf0582e7d218c1498abf160bf9496a1c98
diyanamendoza <diyana.amp@pm.me>
2022-01-23 20:27:35 -0800

    Finish up sarani's test

--- src/components/Cards/CardList.test.jsx
+++ src/components/Cards/CardList.test.jsx
@@ -1,46 +1,40 @@
 import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router';
 import { DeckProvider } from '../../context/DeckContext/DeckContext'
-import CardFront from './CardFront'
+import CardList from './CardList';
 
 const characterList = [{
+   id: 1,
    title: 'Pepe Le Pew',
    animal: 'skunk',
    image:'skunkURL',
    category: 'pronoun' 
 },
 {
+    id: 2,
     title: 'Taz',
-    animal:'tazmanian',
-    image:'tazURL',
+    animal: 'tazmanian',
+    image: 'tazURL',
     category: 'pronoun',
 },
 {
+    id: 3,
     title: 'Tweety',
-    animal:'bird',
-    image:'birdURL',
-    category:'pronoun'
+    animal: 'bird',
+    image: 'birdURL',
+    category: 'pronoun'
 }]
 
-const colors = [{
-    blue: 'blue'
-},
-{
-    purple: 'purple'
-},
-{
-    pink:'pink'
-}]
+const colors = ['red','orange','yellow']
 
 it('renders front card component', () => {
     const { container } = render(
         <DeckProvider>
             <MemoryRouter>
-            <CardFront cards={characterList} raindow={colors}/>
+            <CardList cards={characterList} rainbow={colors}/>
             </MemoryRouter>
         </DeckProvider>
     );
-
 
     expect(container).toMatchSnapshot();
 })
